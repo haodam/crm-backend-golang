86f63edada8395aa0c7ae99a640a4088538978f5
haodam <damanhhao3004.com>
2024-09-03 15:51:10 +0700

    example: logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,22 @@
 package main
 
-import "github.com/haodam/user-backend-golang/pkg/transports/https/routers"
+import (
+	"go.uber.org/zap"
+)
 
 func main() {
 
-	r := routers.NewRouter()
-	err := r.Run()
-	if err != nil {
-		return
-	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	//r := routers.NewRouter()
+	//err := r.Run()
+	//if err != nil {
+	//	return
+	//} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	sugar := zap.NewExample().Sugar()
+	sugar.Infof("Hello name:%s, age:%d", "TipGo", 40) // like fmt.Printf(format,a)
+
+	// logger
+	logger := zap.NewExample()
+	logger.Info("Hello", zap.String("name", "TipGo"), zap.Int("age", 40))
+
 }
