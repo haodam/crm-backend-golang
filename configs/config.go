1d428eaea4736190a5fa93da2723be3bbe5af1ed
haodam <damanhhao3004.com>
2024-09-09 01:32:28 +0700

    refactor: code

--- configs/config.go
+++ configs/config.go
@@ -6,6 +6,14 @@ import (
 )
 
 type ServiceConfigs struct {
+	Host            string `mapstructure:"host"`
+	Port            int    `mapstructure:"port"`
+	Username        string `mapstructure:"username"`
+	Password        string `mapstructure:"password"`
+	Dbname          string `mapstructure:"dbname"`
+	MaxIdleConns    int    `mapstructure:"max_idle_conns"`
+	MaxOpenConns    int    `mapstructure:"max_open_conns"`
+	ConnMaxLifeTime int    `mapstructure:"conn_max_life_time"`
 }
 
 func MustLoadConfig(configPath string) *ServiceConfigs {
