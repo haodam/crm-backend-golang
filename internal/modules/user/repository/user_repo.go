ad81c58d64b18a494a0dc58ebfeba895853d9a67
haodam <damanhhao3004.com>
2024-10-09 17:57:05 +0700

    update: serivce user

--- internal/modules/user/repository/user_repo.go
+++ internal/modules/user/repository/user_repo.go
@@ -1,10 +1,17 @@
 package repository
 
 type IUserRepository interface {
+	GetUserByEmail(email string) bool
 }
 
 type userRepositoryImpl struct{}
 
+var _ IUserRepository = (*userRepositoryImpl)(nil)
+
 func NewUserRepository() IUserRepository {
 	return &userRepositoryImpl{}
+}
+
+func (u *userRepositoryImpl) GetUserByEmail(email string) bool {
+	return false
 }
--- internal/modules/user/usecase/user_service.go
+++ internal/modules/user/usecase/user_service.go
@@ -1,1 +1,11 @@
 package usecase
+
+type IUserService interface {
+}
+type userService struct{}
+
+var _ IUserService = (*userService)(nil)
+
+func NewUserService() IUserService {
+	return &userService{}
+}
